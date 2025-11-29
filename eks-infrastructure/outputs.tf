15c01980269d4907ece0ece81533c0ea38ee26a2
Karan Nagwekar <>
2025-11-29 13:27:36 -0500

    added tfs for eks

---
+++ eks-infrastructure/outputs.tf
@@ -0,0 +1,70 @@
+output "cluster_id" {
+  description = "The name/id of the EKS cluster"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = aws_eks_cluster.main.arn
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_version" {
+  description = "The Kubernetes server version for the cluster"
+  value       = aws_eks_cluster.main.version
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+  sensitive   = true
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_security_group.eks_cluster.id
+}
+
+output "node_security_group_id" {
+  description = "Security group ID of the node groups"
+  value       = aws_security_group.eks_nodes.id
+}
+
+output "node_group_id" {
+  description = "EKS node group id"
+  value       = aws_eks_node_group.main.id
+}
+
+output "node_group_arn" {
+  description = "Amazon Resource Name (ARN) of the EKS Node Group"
+  value       = aws_eks_node_group.main.arn
+}
+
+output "node_group_status" {
+  description = "Status of the EKS Node Group. One of: CREATING, ACTIVE, UPDATING, DELETING, CREATE_FAILED, UPDATE_FAILED, DELETE_FAILED, DEGRADED"
+  value       = aws_eks_node_group.main.status
+}
+
+output "cluster_iam_role_arn" {
+  description = "IAM role ARN of the EKS cluster"
+  value       = aws_iam_role.eks_cluster_role.arn
+}
+
+output "node_iam_role_arn" {
+  description = "IAM role ARN for the node group"
+  value       = aws_iam_role.eks_node_role.arn
+}
+
+output "cloudwatch_log_group_name" {
+  description = "Name of the CloudWatch log group for EKS cluster logs"
+  value       = try(aws_cloudwatch_log_group.eks_cluster[0].name, null)
+}
+
+output "configure_kubectl" {
+  description = "Command to configure kubectl"
+  value       = "aws eks update-kubeconfig --name ${aws_eks_cluster.main.name} --region ${var.aws_region}"
+}
