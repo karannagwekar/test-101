525caf8c2a3000d74bd116d0ddd57f2a0cd505fd
Karan Nagwekar <>
2025-11-30 02:02:12 -0500

    reverted back the default values

--- argocd/variables.tf
+++ argocd/variables.tf
@@ -1,7 +1,7 @@
 variable "kubeconfig_path" {
   description = "Path to kubeconfig file for the target cluster (e.g., ~/.kube/config for k3d or ~/.kube/eks-config for EKS)"
   type        = string
-  default     = "~/.kube/k3d-config"
+  default     = "~/.kube/config"
 }
 
 variable "cluster_type" {
